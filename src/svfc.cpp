ea99a4c477ee7a2fb998c65f1985356287e7077f
Vladimir Semyonov <tempname011@gmail.com>
2023-04-27 09:33:55 +0400

    Checkpoint

--- src/svfc.cpp
+++ src/svfc.cpp
@@ -1,6 +1,7 @@
 #include <cstring>
 #include "common.hpp"
 
+/*
 char const input_cstr[] = R"#(
 EntryPoint: struct {
   as: A[];
@@ -16,7 +17,7 @@ B: struct {
 };
 
 )#";
-/*
+*/
 char const input_cstr[] = R"#(
 EntryPoint: struct {
   structs: StructDefinition[];
@@ -53,6 +54,7 @@ ConcreteType: choice {
   i64;
   f32;
   f64;
+  zero_sized;
   defined: struct {
     top_level_definition_index: U32;
   };
@@ -74,397 +76,493 @@ Type: choice {
   };
 };
 )#";
-*/
 
 namespace grammar {
 
 struct ConcreteType {
-	enum class Which {
-		u8,
-		u16,
-		u32,
-		u64,
-		i8,
-		i16,
-		i32,
-		i64,
-		f32,
-		f64,
-		defined,
-		unresolved,
-	} which;
+  enum class Which {
+    u8,
+    u16,
+    u32,
+    u64,
+    i8,
+    i16,
+    i32,
+    i64,
+    f32,
+    f64,
+    zero_sized,
+    defined,
+    unresolved,
+  } which;
 
-	struct Defined {
-		U32 top_level_definition_index;
-	};
+  struct Defined {
+    U32 top_level_definition_index;
+  };
 
-	struct Unresolved {
-		Range<U8> name;
-	};
-	// This is only necessary before everything was parsed.
-	// After that, a second pass must resolve all names.
+  struct Unresolved {
+    Range<U8> name;
+  };
+  // This is only necessary before everything was parsed.
+  // After that, a second pass must resolve all names.
 
-	union {
-		Defined defined;
-		Unresolved unresolved;
-	};
+  union {
+    Defined defined;
+    Unresolved unresolved;
+  };
 };
 
 struct Type {
-	enum class Which {
-		concrete,
-		pointer,
-		fixed_size_array,
-		flexible_array,
-	} which;
+  enum class Which {
+    concrete,
+    pointer,
+    fixed_size_array,
+    flexible_array,
+  } which;
 
-	struct Concrete {
-		ConcreteType type;
-	};
+  struct Concrete {
+    ConcreteType type;
+  };
 
-	struct Pointer {
-		ConcreteType type;
-	};
+  struct Pointer {
+    ConcreteType type;
+  };
 
-	struct FixedSizeArray {
-		U8 count;
-		ConcreteType element_type;
-	};
+  struct FixedSizeArray {
+    U8 count;
+    ConcreteType element_type;
+  };
 
-	struct FlexibleArray {
-		ConcreteType element_type;
-	};
+  struct FlexibleArray {
+    ConcreteType element_type;
+  };
 
-	union {
-		Concrete concrete;
-		Pointer pointer;
-	};
+  union {
+    Concrete concrete;
+    Pointer pointer;
+  };
 };
 
 struct FieldDefinition {
-	U64 name_hash;
-	Type type;
+  U64 name_hash;
+  Type type;
 };
 
 struct OptionDefinition {
-	U64 name_hash;
-	U8 index;
-	Type type;
+  U64 name_hash;
+  U8 index;
+  Type type;
 };
 
 struct StructDefinition {
-	U64 name_hash;
-	Range<FieldDefinition> fields;
+  U64 name_hash;
+  Range<FieldDefinition> fields;
 };
 
 struct ChoiceDefinition {
-	U64 name_hash;
-	Range<OptionDefinition> options;
+  U64 name_hash;
+  Range<OptionDefinition> options;
 };
 
 struct TopLevelDefinition {
-	enum class Which {
-		a_struct,
-		a_choice,
-	} which;
+  enum class Which {
+    a_struct,
+    a_choice,
+  } which;
 
-	union {
-		StructDefinition a_struct;
-		ChoiceDefinition a_choice;
-	};
+  union {
+    StructDefinition a_struct;
+    ChoiceDefinition a_choice;
+  };
 };
 
 struct Root {
-	Range<StructDefinition> structs;
-	Range<ChoiceDefinition> choices;
+  Range<StructDefinition> structs;
+  Range<ChoiceDefinition> choices;
 };
 
 struct ParserState {
-	U64 cursor;
-	Bool fail;
+  U64 cursor;
+  struct Fail {
+    Bool flag;
+    U64 cursor;
+  } fail;
 };
 
 struct ParserContext {
-	vm::LinearArena *arena;
-	Range<U8> input;
-	ParserState state;
+  vm::LinearArena *arena;
+  Range<U8> input;
+  ParserState state;
 };
 
 using Ctx = ParserContext *;
 
+static inline
+void set_fail(Ctx ctx) {
+  if (!ctx->state.fail.flag) {
+    ctx->state.fail = {
+      .flag = true,
+      .cursor = ctx->state.cursor,
+    };
+  }
+}
+
 void skip_whitespace(Ctx ctx) {
-	while (true) {
-		if (ctx->state.fail || ctx->state.cursor == ctx->input.count) {
-			return;
-		}
+  while (true) {
+    if (ctx->state.cursor == ctx->input.count) {
+      set_fail(ctx);
+      return;
+    }
 
-		auto byte = ctx->input.pointer[ctx->state.cursor];
+    auto byte = ctx->input.pointer[ctx->state.cursor];
 
-		if (byte == ' ' || byte == '\t' || byte == '\r' || byte == '\n') {
-			ctx->state.cursor++;
-		} else {
-		  break;
-		}
-	}
+    if (byte == ' ' || byte == '\t' || byte == '\r' || byte == '\n') {
+      ctx->state.cursor++;
+    } else {
+      break;
+    }
+  }
 }
 
 Range<U8> parse_name(Ctx ctx, Bool is_type) {
-	size_t cursor_start = ctx->state.cursor;
-	if (ctx->state.fail || ctx->state.cursor == ctx->input.count) {
-		ctx->state.fail = 1;
-		return {};
-	}
+  size_t cursor_start = ctx->state.cursor;
+  if (ctx->state.cursor == ctx->input.count) {
+    set_fail(ctx);
+    return {};
+  }
 
-	{
-		auto byte = ctx->input.pointer[ctx->state.cursor];
+  {
+    auto byte = ctx->input.pointer[ctx->state.cursor];
 
-		if (0
-			|| (is_type && byte >= 'A' && byte <= 'Z')
-			|| (!is_type && byte >= 'a' && byte <= 'z')
-		) {
-			ctx->state.cursor++;
-		} else {
-			ctx->state.fail = 1;
-			return {};
-		}
-	}
+    if (0
+      || (is_type && byte >= 'A' && byte <= 'Z')
+      || (!is_type && byte >= 'a' && byte <= 'z')
+    ) {
+      ctx->state.cursor++;
+    } else {
+      set_fail(ctx);
+      return {};
+    }
+  }
 
-	while (true) {
-		if (ctx->state.cursor == ctx->input.count) {
-			break;
-		}
+  while (true) {
+    if (ctx->state.cursor == ctx->input.count) {
+      break;
+    }
 
-		auto byte = ctx->input.pointer[ctx->state.cursor];
+    auto byte = ctx->input.pointer[ctx->state.cursor];
 
-		if (0
-		  || (byte >= 'A' && byte <= 'Z')
-		  || (byte >= 'a' && byte <= 'z')
-		  || (byte >= '0' && byte <= '9')
-			|| byte == '_'
-		) {
-			ctx->state.cursor++;
-		} else {
-			break;
-		}
-	}
+    if (0
+      || (byte >= 'A' && byte <= 'Z')
+      || (byte >= 'a' && byte <= 'z')
+      || (byte >= '0' && byte <= '9')
+      || byte == '_'
+    ) {
+      ctx->state.cursor++;
+    } else {
+      break;
+    }
+  }
 
-	return Range<U8> {
-		.pointer = (ctx->input.pointer + cursor_start),
-		.count = ctx->state.cursor - cursor_start,
-	};
+  return Range<U8> {
+    .pointer = (ctx->input.pointer + cursor_start),
+    .count = ctx->state.cursor - cursor_start,
+  };
 }
 
 Range<U8> parse_type_name(Ctx ctx) { return parse_name(ctx, true); }
 Range<U8> parse_value_name(Ctx ctx) { return parse_name(ctx, false); }
 
 void skip_specific_character(Ctx ctx, U8 ascii_character) {
-	if (ctx->state.fail || ctx->state.cursor == ctx->input.count) {
-		ctx->state.fail = 1;
-		return;
-	}
+  if (ctx->state.cursor == ctx->input.count) {
+    set_fail(ctx);
+    return;
+  }
 
-	auto byte = ctx->input.pointer[ctx->state.cursor];
+  auto byte = ctx->input.pointer[ctx->state.cursor];
 
-	if (byte == ascii_character) {
-		ctx->state.cursor++;
-	} else {
-		ctx->state.fail = 1;
-	}
+  if (byte == ascii_character) {
+    ctx->state.cursor++;
+  } else {
+    set_fail(ctx);
+  }
 }
 
 void skip_specific_cstring(Ctx ctx, char const* cstr) {
-	auto clen = strlen(cstr);
-	if (ctx->state.cursor + clen > ctx->input.count) {
-		ctx->state.fail = 1;
-		return;
-	}
+  auto clen = strlen(cstr);
+  if (ctx->state.cursor + clen > ctx->input.count) {
+    set_fail(ctx);
+    return;
+  }
 
-	for (size_t i = 0; i < clen; i++) {
-		if (ctx->input.pointer[ctx->state.cursor + i] != cstr[i]) {
-			ctx->state.fail = 1;
-		}
-	}
+  for (size_t i = 0; i < clen; i++) {
+    if (ctx->input.pointer[ctx->state.cursor + i] != cstr[i]) {
+      ctx->state.cursor += i;
+      set_fail(ctx);
+    }
+  }
 
-	ctx->state.cursor += clen;
+  ctx->state.cursor += clen;
 }
 
 Type parse_type(Ctx ctx) {
-	auto name = parse_type_name(ctx);
-	skip_whitespace(ctx);
+  auto name = parse_type_name(ctx);
+  skip_whitespace(ctx);
 
-	if (ctx->state.fail || ctx->state.cursor == ctx->input.count) {
-		ctx->state.fail = 1;
-		return {};
-	}
+  if (ctx->state.cursor == ctx->input.count) {
+    set_fail(ctx);
+    return {};
+  }
 
-	auto byte = ctx->input.pointer[ctx->state.cursor];
+  auto byte = ctx->input.pointer[ctx->state.cursor];
 
-	if (byte == '*') {
-		return {
-			.which = Type::Which::pointer,
-			.pointer = {
-				.type = {
-					.which = ConcreteType::Which::unresolved,
-					.unresolved = {
-						.name = name,
-					},
-				},
-			},
-		};
-	} else if (byte == '[') {
-		ctx->state.cursor++;
-		skip_whitespace(ctx);
-		skip_specific_character(ctx, ']'); // TODO: fixed size array
-		return {
-			.which = Type::Which::flexible_array,
-			.pointer = {
-				.type = {
-					.which = ConcreteType::Which::unresolved,
-					.unresolved = {
-						.name = name,
-					},
-				},
-			},
-		};
-	} else {
-		ctx->state.fail = 1;
-		return {};
-	}
+  if (byte == '*') {
+    return {
+      .which = Type::Which::pointer,
+      .pointer = {
+        .type = {
+          .which = ConcreteType::Which::unresolved,
+          .unresolved = {
+            .name = name,
+          },
+        },
+      },
+    };
+  } else if (byte == '[') {
+    ctx->state.cursor++;
+    skip_whitespace(ctx);
+    skip_specific_character(ctx, ']'); // TODO: fixed size array
+    return {
+      .which = Type::Which::flexible_array,
+      .pointer = {
+        .type = {
+          .which = ConcreteType::Which::unresolved,
+          .unresolved = {
+            .name = name,
+          },
+        },
+      },
+    };
+  } else {
+    return {
+      .which = Type::Which::concrete,
+      .concrete = {
+        .type = {
+          .which = ConcreteType::Which::unresolved,
+          .unresolved = {
+            .name = name,
+          },
+        },
+      },
+    };
+  }
+}
+
+U64 compute_name_hash(Range<Byte> name) {
+  auto hash = hash64::begin();
+  hash64::add_bytes(&hash, name);
+  return hash;
 }
 
 void parse_choice(Ctx ctx, ChoiceDefinition *result) {
-	auto definition_name = parse_type_name(ctx);
-	skip_specific_character(ctx, ':');
-	skip_whitespace(ctx);
+  auto definition_name = parse_type_name(ctx);
+  skip_specific_character(ctx, ':');
+  skip_whitespace(ctx);
+  skip_specific_cstring(ctx, "choice");
+  skip_whitespace(ctx);
+  skip_specific_character(ctx, '{');
+  skip_whitespace(ctx);
 
-	ASSERT(false);
-	// TODO: probably mostly same as `parse_struct`
-	// Need to review `parse_struct` again before copying it.
-}
+  DynamicArray<OptionDefinition> dynamic_options = {};
+  ParserState::Fail option_fail = {};
 
-U64 compute_name_hash(Range<U8> name) {
-	auto hash = hash64::begin();
-	hash64::add_bytes(&hash, name);
-	return hash;
+  while (true)
+  {
+    auto saved_state = ctx->state;
+    auto option_name = parse_value_name(ctx);
+
+    Type type;
+    if (!ctx->state.fail.flag) {
+      auto post_name_state = ctx->state;
+      skip_specific_character(ctx, ':');
+      skip_whitespace(ctx);
+      type = parse_type(ctx);
+      if (ctx->state.fail.flag) {
+        ctx->state = post_name_state;
+      } else {
+        type = {
+          .which = Type::Which::concrete,
+          .concrete = {
+            .type = {
+              .which = ConcreteType::Which::zero_sized,
+            },
+          },
+        };
+      }
+    }
+
+    skip_whitespace(ctx);
+    skip_specific_character(ctx, ';');
+    skip_whitespace(ctx);
+
+    if (ctx->state.fail.flag) {
+      option_fail = ctx->state.fail;
+      ctx->state = saved_state;
+      break;
+    } else {
+      dynamic_resize(&dynamic_options, ctx->arena, dynamic_options.count + 1);
+      ASSERT(dynamic_options.pointer);
+      dynamic_options.pointer[dynamic_options.count++] = OptionDefinition {
+        .name_hash = compute_name_hash(option_name),
+        .type = type,
+      };
+    }
+  }
+
+  result->name_hash = compute_name_hash(definition_name);
+  result->options = {
+    .pointer = dynamic_options.pointer,
+    .count = dynamic_options.count,
+  };
+
+  skip_whitespace(ctx);
+  skip_specific_character(ctx, '}');
+  skip_whitespace(ctx);
+  skip_specific_character(ctx, ';');
+
+  if (option_fail.flag && ctx->state.fail.flag) {
+    ctx->state.fail = option_fail;
+  }
 }
 
 void parse_struct(Ctx ctx, StructDefinition *result) {
-	auto definition_name = parse_type_name(ctx);
-	skip_specific_character(ctx, ':');
-	skip_whitespace(ctx);
-	skip_specific_cstring(ctx, "struct");
-	skip_whitespace(ctx);
-	skip_specific_character(ctx, '{');
-	skip_whitespace(ctx);
+  auto definition_name = parse_type_name(ctx);
+  skip_specific_character(ctx, ':');
+  skip_whitespace(ctx);
+  skip_specific_cstring(ctx, "struct");
+  skip_whitespace(ctx);
+  skip_specific_character(ctx, '{');
+  skip_whitespace(ctx);
 
-	if (ctx->state.fail) {
-		return;
-	}
+  DynamicArray<FieldDefinition> dynamic_fields = {};
+  ParserState::Fail field_fail = {};
 
-	DynamicArray<FieldDefinition> dynamic_fields = {};
+  while (true) {
+    auto saved_state = ctx->state;
+    auto field_name = parse_value_name(ctx);
+    skip_specific_character(ctx, ':');
+    skip_whitespace(ctx);
+    auto type = parse_type(ctx);
+    skip_whitespace(ctx);
+    skip_specific_character(ctx, ';');
+    skip_whitespace(ctx);
 
-	while (true) {
-		auto saved_state = ctx->state;
-		auto field_name = parse_value_name(ctx);
-		result->name_hash = compute_name_hash(field_name);
-		skip_specific_character(ctx, ':');
-		skip_whitespace(ctx);
-		auto type = parse_type(ctx);
-		skip_whitespace(ctx);
-		skip_specific_character(ctx, ';');
-		skip_whitespace(ctx);
+    if (ctx->state.fail.flag) {
+      field_fail = ctx->state.fail;
+      ctx->state = saved_state;
+      break;
+    } else {
+      dynamic_resize(&dynamic_fields, ctx->arena, dynamic_fields.count + 1);
+      ASSERT(dynamic_fields.pointer); // temporary
+      dynamic_fields.pointer[dynamic_fields.count++] = FieldDefinition {
+        .name_hash = compute_name_hash(field_name),
+        .type = type,
+      };
+    }
+  }
 
-		if (ctx->state.fail) {
-			ctx->state = saved_state;
-			break;
-		} else {
-			dynamic_resize(&dynamic_fields, ctx->arena, dynamic_fields.count + 1);
-			ASSERT(dynamic_fields.pointer); // temporary
-			dynamic_fields.pointer[dynamic_fields.count++] = FieldDefinition {
-				.name_hash = compute_name_hash(field_name),
-				.type = type,
-			};
-		}
-	}
+  result->name_hash = compute_name_hash(definition_name);
+  result->fields = {
+    .pointer = dynamic_fields.pointer,
+    .count = dynamic_fields.count,
+  };
 
-	result->fields = {
-		.pointer = dynamic_fields.pointer,
-		.count = dynamic_fields.count,
-	};
+  skip_whitespace(ctx);
+  skip_specific_character(ctx, '}');
+  skip_whitespace(ctx);
+  skip_specific_character(ctx, ';');
 
-	skip_whitespace(ctx);
-	skip_specific_character(ctx, '}');
-	skip_whitespace(ctx);
-	skip_specific_character(ctx, ';');
+  if (field_fail.flag && ctx->state.fail.flag) {
+    ctx->state.fail = field_fail;
+  }
 }
 
 TopLevelDefinition *parse_top_level_definition(Ctx ctx) {
-	auto result = vm::allocate_one<TopLevelDefinition>(ctx->arena);
-	ASSERT(result); // temporary
+  auto result = vm::allocate_one<TopLevelDefinition>(ctx->arena);
+  ASSERT(result); // temporary
+  auto saved_state = ctx->state;
 
-	auto saved_state = ctx->state;
-	parse_struct(ctx, &result->a_struct);
+  parse_struct(ctx, &result->a_struct);
+  if (!ctx->state.fail.flag) {
+    result->which = TopLevelDefinition::Which::a_struct;
+    return result;
+  }
 
-	if (!ctx->state.fail) {
-		result->which = TopLevelDefinition::Which::a_struct;
-		return result;
-	}
+  auto struct_fail = ctx->state.fail;
+  ctx->state = saved_state;
 
-	ctx->state = saved_state;
-	parse_choice(ctx, &result->a_choice);
+  parse_choice(ctx, &result->a_choice);
+  if (!ctx->state.fail.flag) {
+    result->which = TopLevelDefinition::Which::a_choice;
+    return result;
+  }
 
-	if (!ctx->state.fail) {
-		result->which = TopLevelDefinition::Which::a_choice;
-		return result;
-	}
+  if (struct_fail.cursor > ctx->state.fail.cursor) {
+    ctx->state.fail = struct_fail;
+  }
 
-	return 0;
+  return 0;
 }
 
 Root *parse_input(vm::LinearArena *arena, Range<U8> input) {
-	ParserContext context_value = {
-		.arena = arena,
-		.input = input,
-		.state = {},
-	};
-	auto ctx = &context_value;
-	auto result = vm::allocate_one<Root>(ctx->arena);
-	ASSERT(result); // temporary
+  ParserContext context_value = {
+    .arena = arena,
+    .input = input,
+    .state = {},
+  };
+  auto ctx = &context_value;
+  auto result = vm::allocate_one<Root>(ctx->arena);
+  ASSERT(result); // temporary
 
-	skip_whitespace(ctx);
-	while (true) {
-		if (ctx->state.cursor == ctx->input.count) {
-			break;
-		}
-		parse_top_level_definition(ctx);
-		skip_whitespace(ctx);
-		if (ctx->state.fail) {
-			return 0;
-		}
-	}
+  skip_whitespace(ctx);
+  while (true) {
+    if (ctx->state.cursor == ctx->input.count) {
+      break;
+    }
+    parse_top_level_definition(ctx);
+    skip_whitespace(ctx);
+    if (ctx->state.fail.flag) {
+      return 0;
+    }
+  }
 
-	return result;
+  return result;
 }
 
 } // namespace grammar
 
 int main(int argc, char *argv[]) {
-	auto arena = vm::create_linear_arena(2ull << 30);
-	// never free, we will just exit the program.
+  auto arena = vm::create_linear_arena(2ull << 30);
+  // never free, we will just exit the program.
 
-	if (!arena.reserved_range.pointer) {
-		printf("Error: could not create main memory arena.\n");
-		return 1;
-	}
+  if (!arena.reserved_range.pointer) {
+    printf("Error: could not create main memory arena.\n");
+    return 1;
+  }
 
-	static_assert(sizeof(char) == 1); // sanity check
-	auto input = Range<U8> {
-		.pointer = (U8 *) input_cstr,
-		.count = strlen(input_cstr),
-	};
+  static_assert(sizeof(char) == 1); // sanity check
+  auto input = Range<U8> {
+    .pointer = (U8 *) input_cstr,
+    .count = strlen(input_cstr),
+  };
 
-	auto result = grammar::parse_input(&arena, input);
+  auto result = grammar::parse_input(&arena, input);
 
-	printf("Success.\n");
-	return 0;
+  if (!result) {
+    printf("Error: failed to parse input.\n");
+    return 1;
+  }
+
+  printf("Success.\n");
+  return 0;
 }
