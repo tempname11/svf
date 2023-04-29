03654ec2d921693adbd02012d20b9f872bf6a8ce
Vladimir Semyonov <tempname011@gmail.com>
2023-04-29 13:59:17 +0400

    Reorder types.

--- src/svfc.cpp
+++ src/svfc.cpp
@@ -1,6 +1,6 @@
 #include <cstring>
+#include <cstdlib>
 #include "common.hpp"
-#include "../test.hpp"
 
 char const input_cstr[] = R"#(
 EntryPoint: struct {
@@ -720,6 +720,11 @@ struct OutputContext {
 
 using Ctx = OutputContext *;
 
+struct OrderElement {
+  size_t index;
+  size_t ordering;
+};
+
 void output_cstring(Ctx ctx, const char *cstr) {
   auto clen = strlen(cstr);
   auto range = vm::allocate_many<Byte>(ctx->dedicated_arena, clen);
@@ -977,7 +982,12 @@ char const *output_cpp_footer = R"(#pragma pack(pop)
 } // namespace svf
 )";
 
-Range<Byte> output_cpp_code(grammar::Root *root, vm::LinearArena *arena, vm::LinearArena *output_arena) {
+Range<Byte> output_cpp_code(
+  grammar::Root *root,
+  Range<OrderElement> ordering,
+  vm::LinearArena *arena,
+  vm::LinearArena *output_arena
+) {
   // We rely on `vm` allocating contiguous memory.
   auto range = vm::allocate_many<Byte>(output_arena, 0);
   OutputContext context_value = {
@@ -991,7 +1001,7 @@ Range<Byte> output_cpp_code(grammar::Root *root, vm::LinearArena *arena, vm::Lin
 
   output_cstring(ctx, "\n// Forward declarations\n");
   for (size_t i = 0; i < root->definitions.count; i++) {
-    auto definition = root->definitions.pointer + i;
+    auto definition = root->definitions.pointer + ordering.pointer[i].index;
     switch (definition->which) {
       case grammar::TopLevelDefinition::Which::a_struct: {
         output_struct_declaration(ctx, &definition->a_struct);
@@ -1009,7 +1019,7 @@ Range<Byte> output_cpp_code(grammar::Root *root, vm::LinearArena *arena, vm::Lin
 
   output_cstring(ctx, "\n// Full declarations\n");
   for (size_t i = 0; i < root->definitions.count; i++) {
-    auto definition = root->definitions.pointer + i;
+    auto definition = root->definitions.pointer + ordering.pointer[i].index;
     switch (definition->which) {
       case grammar::TopLevelDefinition::Which::a_struct: {
         output_struct(ctx, &definition->a_struct);
@@ -1030,7 +1040,118 @@ Range<Byte> output_cpp_code(grammar::Root *root, vm::LinearArena *arena, vm::Lin
   return ctx->range;
 }
 
-} // namespace grammar
+int compare_by_ordering(const void *a_, const void *b_) {
+  auto a = (OrderElement *) a_;
+  auto b = (OrderElement *) b_;
+  if (a->ordering < b->ordering) {
+    return -1;
+  } else if (a->ordering > b->ordering) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+Range<OrderElement> order_types(grammar::Root *root, vm::LinearArena *arena) {
+  auto result = vm::allocate_many<OrderElement>(arena, root->definitions.count);
+  ASSERT(result.pointer);
+
+  for (size_t i = 0; i < root->definitions.count; i++) {
+    result.pointer[i] = {
+      .index = i,
+      .ordering = size_t(-1),
+    };
+  }
+
+  size_t current_ordering = 0;
+
+  for (;;) {
+    bool all_ok = true;
+    bool some_ok = false;
+
+    for (size_t i = 0; i < root->definitions.count; i++) {
+      if (result.pointer[i].ordering < current_ordering) {
+        continue;
+      }
+      bool ok = true;
+      auto definition = root->definitions.pointer + i;
+      switch (definition->which) {
+        case grammar::TopLevelDefinition::Which::a_struct: {
+          for (size_t j = 0; j < definition->a_struct.fields.count; j++) {
+            auto field = definition->a_struct.fields.pointer + j;
+            if (field->type.which != grammar::Type::Which::concrete) {
+              continue;
+            }
+            if (field->type.concrete.type.which != grammar::ConcreteType::Which::defined) {
+              continue;
+            }
+            auto ix = field->type.concrete.type.defined.top_level_definition_index;
+            if (result.pointer[ix].ordering >= current_ordering) {
+              ok = false;
+              break;
+            }
+          }
+          break;
+        }
+        case grammar::TopLevelDefinition::Which::a_choice: {
+          for (size_t j = 0; j < definition->a_choice.options.count; j++) {
+            auto option = definition->a_choice.options.pointer + j;
+            if (option->type.which != grammar::Type::Which::concrete) {
+              continue;
+            }
+            if (option->type.concrete.type.which != grammar::ConcreteType::Which::defined) {
+              continue;
+            }
+            auto ix = option->type.concrete.type.defined.top_level_definition_index;
+            if (result.pointer[ix].ordering >= current_ordering) {
+              ok = false;
+              break;
+            }
+          }
+          break;
+        }
+        default: {
+          ASSERT(false);
+        }
+      }
+      if (ok) {
+        some_ok = true;
+        result.pointer[i].ordering = current_ordering;
+        //printf("Type %zu has ordering %zu\n", i, current_ordering);
+      } else {
+        all_ok = false;
+      }
+    }
+
+    if (all_ok) {
+      break;
+    }
+
+    if (some_ok) {
+      current_ordering++;
+    } else {
+      return {};
+    }
+  }
+
+  /*
+  printf("Ordering:\n");
+  for (size_t i = 0; i < result.count; i++) {
+    printf("%zu: %zu\n", result.pointer[i].index, result.pointer[i].ordering);
+  }
+  */
+
+  qsort(
+    result.pointer,
+    result.count,
+    sizeof(*result.pointer),
+    output::compare_by_ordering
+  );
+
+  return result;
+}
+
+} // namespace output
 
 int main(int argc, char *argv[]) {
   auto arena = vm::create_linear_arena(2ull << 30);
@@ -1047,20 +1168,26 @@ int main(int argc, char *argv[]) {
     .count = strlen(input_cstr),
   };
 
-  auto result = grammar::parse_input(&arena, input);
+  auto root = grammar::parse_input(&arena, input);
 
-  if (!result) {
+  if (!root) {
     printf("Error: failed to parse input.\n");
     return 1;
   }
 
-  if (!grammar::resolve_types(result)) {
+  if (!grammar::resolve_types(root)) {
     printf("Error: failed to resolve types.\n");
     return 1;
   }
 
+  auto ordering = output::order_types(root, &arena);
+  if (!ordering.pointer) {
+    printf("Error: failed to order types.\n");
+    return 1;
+  }
+
   auto output_arena = vm::create_linear_arena(2ull << 30);
-  auto output_range = output::output_cpp_code(result, &arena, &output_arena);
+  auto output_range = output::output_cpp_code(root, ordering, &arena, &output_arena);
 
   /*
   printf("Success.\n");
